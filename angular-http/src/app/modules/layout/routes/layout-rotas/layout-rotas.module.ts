976320db11f00f2ffbc132bef13912942248ef60
uirapeixoto <uira.peixoto@gmail.com>
2020-10-11 18:31:37 -0300

    menu lateral

--- angular-http/src/app/modules/layout/routes/layout-rotas/layout-rotas.module.ts
+++ angular-http/src/app/modules/layout/routes/layout-rotas/layout-rotas.module.ts
@@ -1,16 +1,20 @@
+import { ConfirmacaoComponent } from './../../components/confirmacao/confirmacao.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from '../../components/form/form.component';
 import { TableComponent } from '../../components/table/table.component';
 import { InicioComponent } from '../../components/inicio/inicio.component';
+import { SucessoComponent } from '../../components/sucesso/sucesso.component';
 
 const layouterotas : Routes = [{
   path: '', component: InicioComponent,
   data:{ tittle:'Layout'},
   children:[
     { path:'form', component: FormComponent },
-    { path:'table', component: TableComponent },
+    { path:'tabela', component: TableComponent },
+    { path:'confirmacao', component: ConfirmacaoComponent },
+    { path:'sucesso', component: SucessoComponent },
   ]
 }]
 
