d4fbd5b4275d8b50e23bbefea235627f03ca8813
uirapeixoto <uira.peixoto@gmail.com>
2020-10-17 19:07:08 -0300

    ajustes no datatable

--- angular-http/src/app/modules/layout/components/table/table.component.ts
+++ angular-http/src/app/modules/layout/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Categoria } from 'src/app/models/categoria';
 import { Produto } from 'src/app/models/produto';
 import { CategoriaService } from 'src/app/services/categoria.service';
@@ -11,28 +12,28 @@ import { CategoriaService } from 'src/app/services/categoria.service';
 })
 export class TableComponent implements OnInit {
 
-  categorias: Categoria[];
+  categorias : Categoria[];
   categoria: Categoria;
-  produtos: Produto[];
-  produto: Produto;
   dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<Categoria> = new Subject();
 
-  constructor(private categoriaservice: CategoriaService, private http: HttpClient) { }
+  constructor(private categoriaservice : CategoriaService, private http : HttpClient ) { }
 
   ngOnInit(): void {
     this.dtOptions = {
-      pagingType: 'full_numbers',
-    };
-
+        pagingType: 'full_numbers',
+        lengthMenu: [5,10,20]
+      };
     this.buscarCategorias();
   }
 
-  buscarCategorias() {
+  buscarCategorias(){
     this.categoriaservice
-      .buscarCategorias()
-      .subscribe((categorias: Categoria[]) => {
-        this.categorias = categorias;
-      })
+    .buscarCategorias()
+    .subscribe(( categorias : Categoria[]) => {
+      this.categorias = categorias;
+      this.dtTrigger.next();
+    })
   }
 
 }
